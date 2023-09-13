c3fbcac34cf333328cefc202ddc58057a64146bf
biphan380 <biphan380@gmail.com>
2023-09-12 20:50:49 -0400

    create index and persist to disk

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# NOTE: follow the jupiter notebook file for complete context and instructions. 
+# This main file excludes many of the instructional steps
+# that are shown in the jupiter notebook
 import json
 import os
 
@@ -12,7 +15,23 @@ from llama_index import Document
 # create document objects using doc_ids and dates from each thread
 documents = []
 for thread in threads:
-    thread_text = thread['thread']
-    thread_id = thread['metadata']['id']
-    timestampe = thread['metadata']['timestamp']
-    documents.append(Document(thread_text, doc_id=thread_id, extra_info={'date': timestampe}))
+    thread_text = thread["thread"]
+    thread_id = thread["metadata"]["id"]
+    timestamp = thread["metadata"]["timestamp"]
+    documents.append(
+        Document(text=thread_text, id_=thread_id, metadata={"date": timestamp})
+    )
+
+from llama_index import VectorStoreIndex
+
+# This is saved in-memory (RAM) and not persisted to disk at this time
+index = VectorStoreIndex.from_documents(documents)
+
+print("ref_docs ingested: ", len(index.ref_doc_info))
+print("number of input documents: ", len(documents))
+
+thread_id = threads[0]["metadata"]["id"]
+print(index.ref_doc_info[thread_id])
+
+# We persist to disk
+index.storage_context.persist(persist_dir="./storage")
